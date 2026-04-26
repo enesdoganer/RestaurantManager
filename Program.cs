35433bb52f51e79ef4bafea44dd1e0472fbd64d8
Decadence666 <enesdgnr666@gmail.com>
2026-04-26 17:09:57 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Write(
+    new FigletText("Restaurant Manager")
+        .Centered()
+        .Color(Color.Red));
+
+AnsiConsole.MarkupLine(("[grey]Press any key to continue...[/]"));
+Console.ReadKey();        
